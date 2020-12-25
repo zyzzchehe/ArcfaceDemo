7c16840244c7be0101d5d7ec6211a2a334317902
yazhou zhang <zyzzc123456@gmail.com>
2020-12-25 18:35:10 +0800

    Initial commit

---
+++ app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -0,0 +1,17 @@
+package com.arcsoft.arcfacedemo.common;
+
+public class Constants {
+
+    public static final String APP_ID = "BrB9q6wGexhJYn8eQ6X7iitAZJGGhXvFytR28NbYdv22";
+    public static final String SDK_KEY = "GcbsAUBMgZYLi51eM93EwGG2oSJK4WSXYXtsF8x8zAtK";
+
+    /**
+     * IR预览数据相对于RGB预览数据的横向偏移量，注意：是预览数据，一般的摄像头的预览数据都是 width > height
+     */
+    public static final int HORIZONTAL_OFFSET = 0;
+    /**
+     * IR预览数据相对于RGB预览数据的纵向偏移量，注意：是预览数据，一般的摄像头的预览数据都是 width > height
+     */
+    public static final int VERTICAL_OFFSET = 0;
+}
+
